69105c099fa53174b3a672f67d72f056dec183f3
GugP <pgugninskiy@gmail.com>
2020-12-14 19:33:15 +0300

    Create Module11.Task1.js

---
+++ Module11.Task1.js
@@ -0,0 +1,13 @@
+let arr1 = new Map ([
+  ['Alice','5'],
+  ['Bob','4'],
+  ['Kris','4'],
+  ['Paul','4'],
+  ['Sofie','3']
+]);
+function getNameKeyArr(x){
+	for(let name of x){
+  console.log(name)
+}
+}
+getNameKeyArr(arr1)

c46cff14b536c355e0217368e2c5678e61732a4c
GugP <pgugninskiy@gmail.com>
2021-02-14 23:35:38 +0300

    Update JS.js

--- 14/14.6/JS.js
+++ 14/14.6/JS.js
@@ -1,36 +1,35 @@
-
+// объявляем переменные, приводим их к нужному типу
 let btn = document.getElementById("btn");
 let idUser = document.getElementById("idUser");
 let dataJSON = [];
+let dataId = [];
 
+//пишем обработчик для кнопки
 btn.addEventListener('click', () => {
-  // Делаем запрос за данными
+  // по клику делаем запрос к данным
   fetch('https://jsonplaceholder.typicode.com/users/3/todos')
     .then((response) => {
-      // Объект ответа на запрос
-      // Превращаем объект в JSON. Мы не можем его сразу прочитать,
-      // надо отдать в следующий then
       const result = response.json();
       return result;
     })
     .then((data) => {
       // Объект результата в формате JSON  
       dataJSON = data;
+      // т.к. данные по ИД лежат во вложенном масиве, то получаем отдельный массив ИД
+      for (let i in dataJSON) { dataId.push(dataJSON[i].id) }
+      
+      // делаем проверку массива ИД на наличие введенного значения
+      // если значение есть в массиве, то выводим задачу для этого значени ИД
+      if (dataId.includes(Number(idUser.value))) {
 
-      for (let i in dataJSON) {
-          
-          if (dataJSON[i].id == idUser.value) {
-          div.insertAdjacentHTML('beforeend', `<ol><s>${dataJSON[i].title}</s><ol>`);
-            } else if (dataJSON[i].id != idUser.value) {
-         console.log ("Пользователь с указанным id не найден")
-          } 
-          
-        }
-      }
-      )
+        for (let i in dataJSON) {
+          if (dataId[i] == Number(idUser.value)){div.insertAdjacentHTML('beforeend', `<ol><s>${dataJSON[i].title}</s><ol>`);}
+                 
+                  }}
+      // если значения ИД нет, то выводим соответствующее сообщение 
+      else {div.insertAdjacentHTML('beforeend', `<ol>"Пользователь с указанным id не найден" <ol>`) }
+ 
+        } )
 
-    .catch(() => { console.log('error') });
+    .catch(() => { console.log('error')})  ;
 });
-
-
-

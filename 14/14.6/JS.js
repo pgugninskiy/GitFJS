9dc4649fd2d7a5275a4ed799e2429eb3ffb74bbc
GugP <pgugninskiy@gmail.com>
2021-02-15 23:45:57 +0300

    Update JS.js

--- 14/14.6/JS.js
+++ 14/14.6/JS.js
@@ -1,35 +1,66 @@
-// объявляем переменные, приводим их к нужному типу
+// объявляем переменные
 let btn = document.getElementById("btn");
 let idUser = document.getElementById("idUser");
 let dataJSON = [];
 let dataId = [];
+let str = 'https://jsonplaceholder.typicode.com/users/...';
 
-//пишем обработчик для кнопки
-btn.addEventListener('click', () => {
-  // по клику делаем запрос к данным
-  fetch('https://jsonplaceholder.typicode.com/users/3/todos')
-    .then((response) => {
-      const result = response.json();
-      return result;
-    })
-    .then((data) => {
-      // Объект результата в формате JSON  
-      dataJSON = data;
-      // т.к. данные по ИД лежат во вложенном масиве, то получаем отдельный массив ИД
-      for (let i in dataJSON) { dataId.push(dataJSON[i].id) }
-      
-      // делаем проверку массива ИД на наличие введенного значения
-      // если значение есть в массиве, то выводим задачу для этого значени ИД
-      if (dataId.includes(Number(idUser.value))) {
+// обработчик для кнопки
+btn.addEventListener('click', () => { checkID() });
 
-        for (let i in dataJSON) {
-          if (dataId[i] == Number(idUser.value)){div.insertAdjacentHTML('beforeend', `<ol><s>${dataJSON[i].title}</s><ol>`);}
-                 
-                  }}
-      // если значения ИД нет, то выводим соответствующее сообщение 
-      else {div.insertAdjacentHTML('beforeend', `<ol>"Пользователь с указанным id не найден" <ol>`) }
- 
-        } )
+// функция для проверки ИД пользователя
+function checkID () {
+  // выполняем запрос к списку пользовательских ИД
+  fetch(str.replace('...',``))
+  .then((response) => { return response.json(); })
+  .then((data) => { 
+    // создаем доп массив. не знаю зачем, но без него не работает
+    dataJSON = data
+    // вытаскиваем ИД и заполняем циклом новый массив
+    for (let i in data) { dataId.push(dataJSON[i].id) }  
+    //проверяем специальным методом наличие значения введенного пользователем
+    if (dataId.includes(Number(idUser.value))) {
+    // если всё ОК, то вызываем функцию для получения списка задач пользователя
+         getToDo ()  
+                                        } 
+    // если всё не ОК, то выводим соответствующее сообщение
+    else {div.insertAdjacentHTML('beforeend', `<ol>"Пользователь с указанным id не найден" <ol>`) }             
+                  })
+  .catch(() => { console.log('error') });
+};
 
-    .catch(() => { console.log('error')})  ;
-});
+// функция для получения списка задач пользователя
+function getToDo () {
+    fetch(str.replace('...',`${idUser.value}/todos`))
+    
+      .then((response) => {
+        const result = response.json();
+        return result;
+      })
+      .then((data) => {
+        // Объект результата в формате JSON  
+        dataJSON = data;
+        for (let i in dataJSON) { dataId.push(dataJSON[i].id) }
+
+        // выводим список задач этого пользователя
+        insertToDo (dataJSON)
+        
+           } )
+
+      .catch(() => { console.log('error')})  ;
+};
+// функция для вставки тэгов на страницу
+function insertToDo () {
+  for (let i in dataJSON) {
+          div.insertAdjacentHTML('beforeend', `<ul>`);
+          div.insertAdjacentHTML('beforeend', `<li>"userId":${dataJSON[i].userId}</li>`);
+          div.insertAdjacentHTML('beforeend', `<li>"id":${dataJSON[i].id}</li>`);
+          if (!dataJSON[i].completed) {
+          div.insertAdjacentHTML('beforeend', `<li>"title":${dataJSON[i].title}</li>`);}
+          else {div.insertAdjacentHTML('beforeend', `<li>"title":<s>${dataJSON[i].title}</s></li>`);}
+
+          div.insertAdjacentHTML('beforeend', `<li>"completed":${dataJSON[i].completed}</li>`);  
+          div.insertAdjacentHTML('beforeend', `</ul>`);
+        }
+};
+
